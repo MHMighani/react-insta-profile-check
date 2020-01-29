82ea4dcd0031a5e8aec22b2b69efc4002270bbda
MHMighani <mh.mgh2012@gmail.com>
2020-01-29 17:50:34 +0330

    modal-message for adding or deleting a user added

---
+++ client/src/components/ModalMessage.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Modal from "./Modal";
+import { Link } from "react-router-dom";
+import history from "../history";
+import { deleteUser } from "../api/api";
+import { addNewUserToDatabase as addUser } from "../api/api";
+
+class ModalMessage extends React.Component {
+  actionButton() {
+    const { action, id, username } = this.props.match.params;
+
+    if (action === "delete") {
+      deleteUser(id);
+      history.push("/allusers");
+    } else if (action === "add") {
+      addUser(username);
+      history.push("/add");
+    }
+  }
+
+  renderActions() {
+    return (
+      <React.Fragment>
+        <Link to="/allusers" className="ui button">
+          cancel
+        </Link>
+        <button
+          className="ui button negative"
+          onClick={() => this.actionButton()}
+        >
+          {this.props.match.params.action}
+        </button>
+      </React.Fragment>
+    );
+  }
+
+  renderContent(action, username) {
+    if (action === "add") {
+      return `are you sure you want to Add ${username} ?`;
+    } else if (action === "delete") {
+      return `are you sure you want to delete ${username}?`;
+    }
+    return "are you sure you want to do this?";
+  }
+
+  renderTitle(action) {
+    if (action === "add") {
+      return "Add user";
+    } else if (action === "delete") {
+      return "Delete user";
+    }
+    return "are you sure?";
+  }
+  render() {
+    const { action, username, id } = this.props.match.params;
+
+    return (
+      <Modal
+        title={this.renderTitle(action)}
+        content={this.renderContent(action, username)}
+        actions={this.renderActions()}
+        onDismiss={() => history.push("/allusers")}
+      />
+    );
+  }
+}
+
+export default ModalMessage;
